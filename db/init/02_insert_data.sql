1c72f0e9c81477617dad655b74f9ad7360f87696
yumiyumida4 <holyspiritsfire@gmail.com>
2021-01-16 22:50:14 +0900

    19th commit

---
+++ db/init/02_insert_data.sql
@@ -0,0 +1,5 @@
+INSERT INTO book VALUES(DEFAULT, 'プログラミング言語C', current_timestamp);
+INSERT INTO book VALUES(DEFAULT, 'やさしいコンピューター科学', current_timestamp);
+INSERT INTO book VALUES(DEFAULT, 'ゲーテル、エッシャー、バッハーあるいは不思議の環', current_timestamp);
+INSERT INTO book VALUES(DEFAULT, 'Texブック　コンピューターによる組版システム', current_timestamp);
+INSERT INTO book VALUES(DEFAULT, '人月の神話　サル人間を撃つ銀の弾はない', current_timestamp);
